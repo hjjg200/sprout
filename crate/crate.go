e6d378eef58cf5cfe2cba39d4765a073032dee98
anton <anton@nikeagames.com>
2019-02-11 17:24:50 +0900

    WIP - making type crate

--- crate/crate.go
+++ crate/crate.go
@@ -1,5 +1,28 @@
 package crate
 
 type Crate struct {
-    
+    assets    map[string] *Asset
+    localizer localizer.Localizer
+    templates *template.Template
+    mod_time  map[string] time.Time
+
+    default_locale string
 }
+
+const (
+    envDirAsset = "asset"
+    envDirLocale = "locale"
+    envDirTemplate = "template"
+)
+
+/*
+funcs to create
+
+MakeArchive
+FromArchive
+FromDirectory
+Asset( string ) *Asset, bool
+Template( string ) *Template, bool
+Localize( string, string, int ) string, error
+
+*/
