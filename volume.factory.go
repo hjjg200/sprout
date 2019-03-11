af53ce15ae4fe722961406074149688695b77a84
anton <anton@nikeagames.com>
2019-03-11 18:07:51 +0900

    Lost -- need to organize the to do list

--- volume.factory.go
+++ volume.factory.go
@@ -10,5 +10,66 @@ type volume_factory struct {}
 var  static_volume_factory = &volume_factory{}
 
 func VolumeFactory() *volume_factory {}
-func( _volfac *volume_factory ) FromSource( _path string ) ( *Volume, error ) {}
+func( _volfac *volume_factory ) FromDirectory( _path string ) ( *Volume, error ) {
+
+    _volume := &Volume{
+        assets: make( map[string] *Asset ),
+        localizer: LocaleFactory().New(),
+        templates: template.New( "" ),
+        hold_group: together.NewHoldGroup(),
+    }
+
+    _walk_func := func( _2path string, _2info os.FileInfo, _2error error ) error {
+        switch {
+        case strings.HasPrefix( _2path, VolumeVariables().ConstAssetDirectory() + "/" ):
+
+        case strings.HasPrefix( _2path, VolumeVariables().ConstLocaleDirectory() + "/" ):
+            // Check extension
+            _2extension := filepath.Ext( _2path )
+            if _2extension == VolumeVariables().ConstLocaleExtension() {
+                // Read file
+                _2file, _2locale_error := os.Open( _2path )
+                if _2locale_error != nil {
+                    return _2locale_error
+                }
+                _2buffer := bytes.NewBuffer()
+                io.Copy( _2buffer, _2file )
+                _2file.Close()
+                // Add locale
+                _2locale, _2locale_error := LocaleFactory().FromJSON( _2buffer.Bytes() )
+                if _2locale_error != nil {
+                    return _2locale_error
+                }
+                _volume.localizer.AddLocale( _2locale )
+            }
+        case strings.HasPrefix( _2path, VolumeVariables().ConstTemplateDirectory() + "/" ):
+            // Chcek extension
+            _2found := false
+            _2extension = filepath.Ext( _2path )
+            for _, _2value := range VolumeVariables().TemplateExtensions() {
+                if _2value == _2extension {
+                    _2found = true
+                }
+            }
+            if _2found == false {
+                return nil
+            }
+            // Read file
+            _2file, _2template_error := os.Open( _2path )
+            if _2template_error != nil {
+                return _2template_error
+            }
+            _2buffer := bytes.NewBuffer()
+            io.Copy( _2buffer, _2file )
+            _2file.Close()
+            // Add to templates
+            _, _2template_error = _volume.templates.New( _2path ).Parse( _2buffer.String() )
+            if _2template_error != nil {
+                return _2template_error
+            }
+        }
+        return nil
+    }
+
+}
 func( _volfac *volume_factory ) New() ( *Volume ) {}
--- volume.variables.go
+++ volume.variables.go
@@ -11,9 +11,13 @@ type volume_variables struct {
     template_right_delimiter string
     cache_name_format string
     max_asset_size int64 // in bytes
+    whitelisted_extensions []string
 
-    // DEFAULT DEFINITIONS
-    default_whitelisted_extensions []string
+    // CONSTANT DEFINITIONS
+    const_asset_directory string
+    const_template_directory string
+    const_locale_directory string
+    const_locale_extension string
 
     // ERROR DEFINITIONS
     error_no_available_cache error
@@ -27,11 +31,15 @@ var  static_volume_variables = &volume_variables{
     template_right_delimiter: "}}",
     cache_name_format: "cache-%d",
     max_asset_size: 10 << 20, // 10MB
-
-    // DEFAULT DEFINITIONS
-    default_whitelisted_extensions: []string{
+    whitelisted_extensions: []string{
         ".css", ".js", ".jpg", ".jpeg", ".png", ".gif", ".ico", ".icn", ".html", ".htm",
     },
+
+    // CONSTANT DEFINITIONS
+    const_asset_directory: "asset",
+    const_template_directory: "tempplate",
+    const_locale_directory: "locale",
+    const_locale_extension: ".json",
 
     // ERROR DEFINITIONS
     error_no_available_cache: Error{
